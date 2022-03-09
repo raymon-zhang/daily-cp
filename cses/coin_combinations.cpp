32f6e7af166a01b10beb6dcdce511298e7633e3f
Raymon Zhang <raymonzhang.rz@gmail.com>
2022-03-08 19:54:50 -0800

    Create coin_combinations.cpp

---
+++ cses/coin_combinations.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+const int MOD = 1e9+7;
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+ 
+    int n, x;
+    cin >> n >> x;
+ 
+    vector<int> coins;
+    for (int i = 0; i < n; i++) {
+        int y;
+        cin >> y;
+        coins.push_back(y);
+    }
+ 
+    vector<int> dp(x+1);
+    dp[0] = 1;
+ 
+    for (int i = 1; i <= x; i++) {
+        for (int c: coins) {
+            if (i-c >= 0) {
+                dp[i] += dp[i-c];
+                dp[i] %= MOD;
+            }
+        }
+    }
+ 
+    cout << dp[x] << "\n";
+}
